109342b5985aff11c575ccc8ad7d047362c30d15
ichika <113494974+ichika99@users.noreply.github.com>
2022-10-28 13:33:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ window.Config = {
   ],
 
   // 日志天数
-  CountDays: 90,
+  CountDays: 60,
 
   // 是否显示检测站点的链接
   ShowLink: true,
@@ -18,8 +18,7 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://ichika.cc/'
+
     }
   ],
 };
