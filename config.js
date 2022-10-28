66c7e7cd560a911506bb764f1c01ed449a5a1a25
ichika <113494974+ichika99@users.noreply.github.com>
2022-10-28 14:49:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm792924094-50ced8d30a7f866a7a3778c5'
+    'm792924094-50ced8d30a7f866a7a3778c5',
+    'm792943834-fd3142b775bbfc39f8bad490'
   ],
 
   // 日志天数
